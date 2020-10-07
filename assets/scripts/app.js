3307c05fd40ac3e80c20123dc652118e25bcf43f
Ishmam <techopoly444@gmail.com>
2020-10-07 20:49:00 +0600

    16

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,20 @@
-//18-15
-/* lets work on fetch api.
-you will not understand everything about fetch api but the main concept is that fetch() is a promisified function
-by default that the browser supports.
--fetch(url) sends the request to the server and recieves response.
--you can access the response using then() as you do in other promisified async functions
--then convert the response from streamed json body to workable javascript snapshot with response.
- */
+//18-16
+//lets work on how to send post request to the server.
+/* as we do in the httpXMLRequest, we will add an object in the argument in the fetch() to define the method */
+
 const liElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post')
 
 
 function sendHttpRequest(method, url, data) {
-    return fetch(url).then(response => { // eventually returns the parsed response data
+
+    return fetch(url, {  // returns a promise
+        method: method,
+        body: JSON.stringify(data)
+    }).then(response => { // eventually returns the parsed response data
         return response.json()
     })
 }
-
 
 async function fetchPosts() {
     try {
@@ -56,28 +55,3 @@ createPost('how to get into relationship', 'this comes with experience and refle
 
 
 
-
-// this is how I did
-/* lets try something. lets promisify the onload */
-
-/* function afterLoad() {
-    const promise = new Promise((res, rej) => {
-        xhr.onload = function () {
-            //intoArray = JSON.parse(xhr.response) // converting json into array
-            //console.log(intoArray)
-            //console.log(xhr.response)
-            res(xhr.response);
-        };
-    })
-    return promise
-}
-
-afterLoad().then(posts => {
-    console.log(posts);
-    for (const post of posts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        liElement.append(postEl);
-    }
-}) */
